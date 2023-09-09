5bc263cf0b7e3d980544634b0be0408954d10004
saiumesh23 <119828269+saiumesh23@users.noreply.github.com>
2023-09-09 11:06:38 +0530

    Create isprime.cpp
     a function to check whether a number is prime or not .the constaint is 1<n<10^18

---
+++ isprime.cpp
@@ -0,0 +1,9 @@
+bool isprime(ll n){
+ if (n==1) return false;
+if(n==2||n==3) return true;
+if(n%2==0 || n%3==0) return false;
+for(ll i=5;i*i<=n;i=i+6){
+if(n%i==0) return false;
+}
+return true;
+}
